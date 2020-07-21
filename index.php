74d188b0cfdb689e9d448f1cd2b2ce3c623d1055
Vadimir <45332930+FirstHard@users.noreply.github.com>
2020-07-21 09:03:04 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
-
 	define("iINDEX", true);
-	//require_once 'config.php';
-
-	//print_r($database);
-
+	require_once 'config.php';
 	require_once 'app/link.php';
-
 	include('app/router.php');
 	include('app/controller.php');
-
 ?>
